a88c7a34eb348de8318723a1b0bb3c589d5f889a
venzy <david.venz@gmail.com>
2025-08-15 22:19:29 +1000

    Ch2 Message Brokers L4 Connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+    amqp "github.com/rabbitmq/amqp091-go"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer connection.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	// Create a context that cancels on SIGINT or SIGTERM
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	fmt.Println("Running... Press Ctrl+C to exit.")
+
+	// Block until signal is received - Done() returns a channel that is closed when the context is canceled
+	<-ctx.Done()
+
+	fmt.Println("Received shutdown signal, cleaning up...")
 }
