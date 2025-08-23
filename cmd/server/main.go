64735f5083360b5bc4644b9def9ead2655bc3f71
venzy <david.venz@gmail.com>
2025-08-23 21:08:12 +1000

    Ch3 Publishers & Queues L6 Decoupling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -40,6 +41,34 @@ func main() {
 	defer stop()
 
 	fmt.Println("Running... Press Ctrl+C to exit.")
+
+	// Hint to the user about server commands
+	gamelogic.PrintServerHelp()
+
+	quit := false
+	for !quit {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		command := userInput[0]
+		switch command {
+		case "pause":
+			fmt.Println("Pausing the game...")
+			playingState.IsPaused = true
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, playingState)
+		case "resume":
+			fmt.Println("Resuming the game...")
+			playingState.IsPaused = false
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, playingState)
+		case "quit":
+			fmt.Println("Quitting the server...")
+			quit = true
+		default:
+			fmt.Printf("Unknown command: %s\n", command)
+			gamelogic.PrintServerHelp()
+		}
+	}
 
 	// Block until signal is received - Done() returns a channel that is closed when the context is canceled
 	<-ctx.Done()
